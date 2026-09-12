f2a3d7d128d0c8ebacba0db161af9c858d6ab30d
xzm7358 <xzm7358@outlook.com>
2026-09-12 16:13:46 -0700

    NS-HARD-001: expose udhcpc process truth

--- src/platform/include/platform/udhcpc_process.h
+++ src/platform/include/platform/udhcpc_process.h
@@ -9,6 +9,7 @@ class UdhcpcProcess {
 public:
     static bool start(const std::string &iface, std::string &error);
     static void stop(const std::string &iface);
+    static bool is_running(const std::string &iface);
 
     static std::string pidfile_for(const std::string &iface);
     static std::string event_script_path(const std::string &iface,
