5ca8ed0ad1a61a80aa0fa73b61a16560feb9047a
xzm7358 <xzm7358@outlook.com>
2026-09-11 21:32:30 -0700

    NS-ARCH-002: add Wi-Fi DHCP lifecycle policy owner

---
+++ src/service/wifi_manager.h
@@ -0,0 +1,40 @@
+#ifndef NETWORK_SERVICE_WIFI_MANAGER_H
+#define NETWORK_SERVICE_WIFI_MANAGER_H
+
+#include <cstdint>
+#include <functional>
+#include <mutex>
+#include <string>
+
+namespace network_service {
+
+struct WifiManagerState {
+    bool l2_connected = false;
+    bool dhcp_requested = false;
+    std::uint32_t dhcp_requests = 0;
+    std::string failure_reason;
+};
+
+class WifiManager {
+public:
+    using DhcpStartFn = std::function<bool(std::string &error)>;
+    using DhcpStopFn = std::function<void()>;
+
+    WifiManager(DhcpStartFn dhcp_start, DhcpStopFn dhcp_stop);
+
+    void on_l2_connected();
+    void on_l2_disconnected();
+    void stop_dhcp();
+
+    WifiManagerState state() const;
+
+private:
+    DhcpStartFn dhcp_start_;
+    DhcpStopFn dhcp_stop_;
+    mutable std::mutex lock_;
+    WifiManagerState state_;
+};
+
+} // namespace network_service
+
+#endif // NETWORK_SERVICE_WIFI_MANAGER_H
